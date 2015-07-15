831e3b6aa6c7939fcc24946f454cf0f9d5e8f76f
EvgeniZhibrik <evgeni.zhibrik@gmail.com>
2015-07-15 19:12:28 +0300

    Main webpage is almoust ready

--- app/assets/js/myscript.js
+++ app/assets/js/myscript.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+  $('[data-toggle="offcanvas1"]').click(function () {
+    $('.row-offcanvas').toggleClass('active1')
+  });
+   $('[data-toggle="offcanvas2"]').click(function () {
+    $('.row-offcanvas').toggleClass('active2')
   });
 });
