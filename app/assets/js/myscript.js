61618c2ef6bdde25f75df6f748b445e470f3508d
EvgeniZhibrik <evgeni.zhibrik@gmail.com>
2015-07-16 18:38:52 +0300

    Registration form added

--- app/assets/js/myscript.js
+++ app/assets/js/myscript.js
@@ -9,4 +9,22 @@ $(document).ready(function () {
 		$('#login-form').hide();
 		$('#main-container').show();
 	});
+	$('#register').click(function(){
+		$('#login-form').hide();
+		$('#registration-form').show();
+	});
+	$('#registration').click(function(){
+		$('#registration-form').hide();
+		$('#main-container').show();
+	});
 });
+var myMap;
+
+function init(){     
+    myMap = new ymaps.Map("map-container", {
+        center: [53.901090 , 27.558759],
+        zoom: 11
+    });
+}
+
+ymaps.ready(init);
