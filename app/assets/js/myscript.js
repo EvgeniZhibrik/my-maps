55e3b86c7d6b23fac319a34e8e5897083478ed8b
EvgeniZhibrik <evgeni.zhibrik@gmail.com>
2015-07-16 16:33:59 +0300

    Login-form ready

--- app/assets/js/myscript.js
+++ app/assets/js/myscript.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-  $('[data-toggle="offcanvas1"]').click(function () {
-    $('.row-offcanvas').toggleClass('active1')
-  });
-   $('[data-toggle="offcanvas2"]').click(function () {
-    $('.row-offcanvas').toggleClass('active2')
-  });
+	$('[data-toggle="offcanvas1"]').click(function () {
+		$('.row-offcanvas').toggleClass('active1')
+	});
+	$('[data-toggle="offcanvas2"]').click(function () {
+		$('.row-offcanvas').toggleClass('active2')
+	});
+	$('#sign-in').click(function () {
+		$('#login-form').hide();
+		$('#main-container').show();
+	});
 });
