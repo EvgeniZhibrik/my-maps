294512e9a48bdec03f13e88695c65380ded36e2c
EvgeniZhibrik <evgeni.zhibrik@gmail.com>
2015-07-23 17:40:22 +0300

    Database design finished

--- server/server.js
+++ server/server.js
@@ -20,116 +20,249 @@ mongoose.connect(db.url);
  
 app.use(bodyParser.urlencoded({ extended: true }));
  
-addAPIRouter(app, mongoose, stormpath);
+
 
 app.use(function(req, res, next){
    res.status(404);
    res.json({ error: 'Invalid URL' });
 });
 
+
+var userSchema = new mongoose.Schema({
+        active: Boolean,
+        email: { type: String, trim: true, lowercase: true },
+        firstName: { type: String, trim: true },
+        lastName: { type: String, trim: true },
+        avatar: { type: String, trim: true },
+        friends: { type: [mongoose.Schema.Types.ObjectId], default: [] },
+        created: { type: Date, default: Date.now },
+        lastLogin: { type: Date, default: Date.now },
+        details: mongoose.Schema.Types.Mixed
+    },
+    {
+        collection: 'user' 
+    }
+);
+
+var UserModel = mongoose.model( 'User', userSchema );
+
+var cafeSchema = new mongoose.Schema({
+        name: {type: String, trim: true},
+        website: {type: String, trim: true, default: ''},
+        coordinates: {
+            latitude: {type: Number},
+            longitude: {type: Number}
+        },
+        description: {type: String, trim: true},
+        publishedBy: { type: mongoose.Schema.Types.ObjectId },
+        published: { type: Date},
+        menu: mongoose.Schema.Types.Mixed
+    },
+    {
+        collection: 'cafe'
+    }
+);
+ 
+ var CafeModel = mongoose.model( 'Cafe', cafeSchema );
+
+var fotoSchema = new mongoose.Schema({
+        description: { type: String, trim: true },
+        title: { type: String, trim: true },
+        cafeID: { type: mongoose.Schema.Types.ObjectId },
+        publishedBy: {type: mongoose.Schema.Types.ObjectId},
+        published: { type: Date },
+        link: { type: String, trim: true  }
+    },
+    {
+        collection: 'foto' 
+    }
+);
+ 
+var FotoModel = mongoose.model( 'Foto', fotoSchema );
+
+var cafeCommentSchema = new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        cafeID: { type: mongoose.Schema.Types.ObjectId },
+        text: { type: String, trim: true},
+        date: { type: Date }
+    },
+    {
+        collection: 'cafeComment' 
+    }
+ );
+
+var CafeCommentModel = mongoose.model('CafeComment', cafeCommentSchema );
+
+var marksSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        cafeID: { type: mongoose.Schema.Types.ObjectId },
+        category: { type: mongoose.Schema.Types.ObjectId }, 
+        mark: { type: Number }
+    },
+    {
+        collection: 'marks' 
+    }
+ );
+
+var MarksModel = mongoose.model('Marks', marksSchema);
+
+var favoritesSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        cafeID: { type: mongoose.Schema.Types.ObjectId },
+    },
+    {
+        collection: 'favorites' 
+    }
+ );
+
+var FavoritesModel = mongoose.model( 'Favorites', favoritesSchema);
+
+var fotoCommentSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        fotoID: { type: mongoose.Schema.Types.ObjectId },
+        text: { type: String, trim: true}, 
+        date: { type: Date }
+    },
+    {
+        collection: 'fotoComment' 
+    }
+ );
+
+var FotoCommentModel = mongoose.model( 'FotoComment', fotoCommentSchema);
+
+
+var cafeCommentCommentSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        cafeCommentID: { type: mongoose.Schema.Types.ObjectId },
+        text: {type: String, trim: true},
+        date: { type: Date }
+    },
+    {
+        collection: 'cafeCommentComment' 
+    }
+ );
+
+var CafeCommentCommentModel = mongoose.model( 'CafeCommentComment', cafeCommentCommentSchema);
+
+var newsSchema =  new mongoose.Schema({
+        text: { type: String, trim: true},
+        link: { type: String, trim: true},
+        date: { type: Date }
+    },
+    {
+        collection: 'news' 
+    }
+ );
+
+var NewsModel = mongoose.model( 'News', newsSchema);
+
+var newsCommentSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        newsID: { type: mongoose.Schema.Types.ObjectId },
+        text: { type: String, trim: true},
+        date: { type: Date }
+    },
+    {
+        collection: 'newsComment' 
+    }
+ );
+
+var NewsCommentModel = mongoose.model( 'NewsComment', newsCommentSchema);
+
+var userFotoSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        fotoID: { type: mongoose.Schema.Types.ObjectId }
+    },
+    {
+        collection: 'userFoto' 
+    }
+ );
+
+var UserFotoModel = mongoose.model( 'UserFoto', userFotoSchema);
+
+var newsCafeSchema =  new mongoose.Schema({
+        newsID: { type: mongoose.Schema.Types.ObjectId },
+        cafeID: { type: mongoose.Schema.Types.ObjectId }
+    },
+    {
+        collection: 'newsCafe' 
+    }
+ );
+
+var NewsCafeModel = mongoose.model( 'NewsCafe', newsCafeSchema);
+
+var discussionSchema = new mongoose.Schema({
+        title: { type: String, trim: true},
+        createdBy: { type: mongoose.Schema.Types.ObjectId},
+        created: {type: Date, default: Date.now}
+    },
+    {
+        collection: 'discussion' 
+    }
+ );
+
+var DiscussionModel = mongoose.model( 'Discussion', discussionSchema);
+
+var discussionCommentSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        discussionID: { type: mongoose.Schema.Types.ObjectId },
+        text: { type: String, trim: true},
+        date: { type: Date }
+    },
+    {
+        collection: 'discussionComment' 
+    }
+ );
+
+var DiscussionCommentModel = mongoose.model( 'DiscussionComment', discussionCommentSchema);
+
+var userDiscussionSchema =  new mongoose.Schema({
+        userID: { type: mongoose.Schema.Types.ObjectId },
+        discussionID: { type: mongoose.Schema.Types.ObjectId }
+    },
+    {
+        collection: 'userDiscussion' 
+    }
+ );
+
+var UserDiscussionModel = mongoose.model( 'UserDiscussion', userDiscussionSchema);
+
+var rankingSchema =  new mongoose.Schema({
+        category: { type: String, trim: true},
+        mark: {type: Number }
+    },
+    {
+        collection: 'ranking' 
+    }
+ );
+
+var RankingModel = mongoose.model( 'Ranking', rankingSchema);
+
+
+app.get('/*', function(req, res, next) {
+    res.contentType('application/json');
+    next();
+});
+app.post('/*', function(req, res, next) {
+    res.contentType('application/json');
+    next();
+});
+app.put('/*', function(req, res, next) {
+    res.contentType('application/json');
+    next();
+});
+app.delete('/*', function(req, res, next) {
+    res.contentType('application/json');
+    next();
+});
+
+var router = express.Router();
+    
+app.use('/api/v1.0', router);
+
+
+
 app.listen(port);
 
 console.log('Magic happens on port ' + port);
  
-exports = module.exports = app;
-
-var userSchema = new mongoose.Schema({
-         active: Boolean,
-         email: { type: String, trim: true, lowercase: true },
-         firstName: { type: String, trim: true },
-         lastName: { type: String, trim: true },
-         sp_api_key_id: { type: String, trim: true },
-         sp_api_key_secret: { type: String, trim: true },
-         subs: { type: [mongoose.Schema.Types.ObjectId], default: [] },
-         created: { type: Date, default: Date.now },
-         lastLogin: { type: Date, default: Date.now },
-     },
-     { collection: 'user' }
-);
-
-userSchema.index({email : 1}, {unique:true});
-userSchema.index({sp_api_key_id : 1}, {unique:true});
-
-var UserModel = mongoose.model( 'User', userSchema );
-
-var feedSchema = new mongoose.Schema({
-         feedURL: { type: String, trim:true },
-         link: { type: String, trim:true },
-         description: { type: String, trim:true },
-         state: { type: String, trim:true, lowercase:true, default: 'new' },
-         createdDate: { type: Date, default: Date.now },
-         modifiedDate: { type: Date, default: Date.now },
-     },
-     { collection: 'feed' }
-);
- 
-feedSchema.index({feedURL : 1}, {unique:true});
-feedSchema.index({link : 1}, {unique:true, sparse:true});
- 
-var FeedModel = mongoose.model( 'Feed', feedSchema );
-
-var feedEntrySchema = new mongoose.Schema({
-         description: { type: String, trim:true },
-         title: { type: String, trim:true },
-         summary: { type: String, trim:true },
-         entryID: { type: String, trim:true },
-         publishedDate: { type: Date },
-         link: { type: String, trim:true  },
-         feedID: { type: mongoose.Schema.Types.ObjectId },
-         state: { type: String, trim:true, lowercase:true, default: 'new' },
-         created: { type: Date, default: Date.now },
-     },
-     { collection: 'feedEntry' }
-);
- 
-feedEntrySchema.index({entryID : 1});
-feedEntrySchema.index({feedID : 1});
- 
-var FeedEntryModel = mongoose.model( 'FeedEntry', feedEntrySchema );
-
-var userFeedEntrySchema = new mongoose.Schema({
-         userID: { type: mongoose.Schema.Types.ObjectId },
-         feedEntryID: { type: mongoose.Schema.Types.ObjectId },
-         feedID: { type: mongoose.Schema.Types.ObjectId },
-         read : { type: Boolean, default: false },
-     },
-     { collection: 'userFeedEntry' }
- );
-
-userFeedEntrySchema.index({userID : 1, feedID : 1, feedEntryID : 1, read : 1});
- 
-var UserFeedEntryModel = mongoose.model('UserFeedEntry', userFeedEntrySchema );
-
-function addAPIRouter(app, mongoose, stormpath) {
- 
-    app.get('/*', function(req, res, next) {
-        res.contentType('application/json');
-        next();
-    });
-    app.post('/*', function(req, res, next) {
-        res.contentType('application/json');
-        next();
-    });
-    app.put('/*', function(req, res, next) {
-        res.contentType('application/json');
-        next();
-    });
-    app.delete('/*', function(req, res, next) {
-        res.contentType('application/json');
-        next();
-    });
-
-    var router = express.Router();
-    
-    router.post('/user/enroll', function(req, res) {
-        logger.debug('Router for /user/enroll');
-    });
-
-    router.get('/feeds', stormpath.apiAuthenticationRequired, function(req, res) {
-        logger.debug('Router for /feeds');
-    });
-    router.put('/feeds/subscribe', stormpath.apiAuthenticationRequired, function(req, res) {
-       logger.debug('Router for /feeds');
-    });
-    app.use('/api/v1.0', router);
-};
